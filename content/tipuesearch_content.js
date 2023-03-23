6e40756b39961029576e43537ca62466e9ca63a1
Martin3130 <nfu40923130@gmail.com>
2023-03-23 16:03:22 +0800

    remove

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n \n', 'tags': '', 'url': 'About.html'}, {'title': 'bpj26', 'text': '', 'tags': '', 'url': 'bpj26.html'}, {'title': 'bpj7', 'text': '', 'tags': '', 'url': 'bpj7.html'}, {'title': 'bpj16', 'text': '', 'tags': '', 'url': 'bpj16.html'}, {'title': 'bpj6', 'text': '今天弄不完，禮拜六再用好了', 'tags': '', 'url': 'bpj6.html'}]};
+=======
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n \n', 'tags': '', 'url': 'About.html'}, {'title': 'bpj7', 'text': '', 'tags': '', 'url': 'bpj7.html'}, {'title': 'bpj16', 'text': '今天弄不完，禮拜六再用好了', 'tags': '', 'url': 'bpj16.html'}]};
+>>>>>>> e8fc24b (remove)
